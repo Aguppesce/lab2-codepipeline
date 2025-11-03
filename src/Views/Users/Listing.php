72b7fc4b735a69d8d81bf698b5b3fea859d639c5
Agustin Perez Pesce <aguppesce@gmail.com>
2025-11-03 13:36:39 +0000

    add: new style

--- src/Views/Users/Listing.php
+++ src/Views/Users/Listing.php
@@ -21,7 +21,7 @@ class Listing
            -webkit-background-clip: text; 
            -webkit-text-fill-color: transparent;
            background-clip: text;">
-                        🚀 The Newcomers - Lab2 Final
+                        🚀 The Newcomers - Laboratorio 2 🚀
                     </h1>
                     <?php
                     /** @var \Entity\User $user */
